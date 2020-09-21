1e0285be5051fea6e284f15306c9e56514733e33
Swamita <swamita2001@gmail.com>
2020-09-21 15:55:10 +0530

    Add check mark Accessory

--- todo/ListViewController.swift
+++ todo/ListViewController.swift
@@ -27,6 +27,18 @@ class ListViewController: UITableViewController {
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
 
 }
